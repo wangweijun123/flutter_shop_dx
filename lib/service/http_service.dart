e2a09dfcd64927df2801476c947c09882714ea12
jntm <262448350@qq.com>
2024-08-25 21:34:00 +0800

    客户端使用ip访问服务端，服务也使用ip启动服务 去掉contentType

--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -7,20 +7,23 @@ Future request(url, {formData}) async {
   try {
     Response response;
     Dio dio = Dio();
-
-    dio.options.contentType =
-        ContentType.parse('application/x-www-form-urlencoded') as String?;
+    // 多余的跑异常
+    // dio.options.contentType =
+    //     ContentType.parse('application/x-www-form-urlencoded') as String?;
     if (formData == null) {
       response = await dio.post(servicePath[url]!);
     } else {
       response = await dio.post(servicePath[url]!, data: formData);
     }
     if (response.statusCode == 200) {
+      print('request url success... ${servicePath[url]} ');
       return response;
     } else {
+      print('request url falied... ${servicePath[url]} ');
       throw Exception('后端接口异常,请检查测试代码和服务器运行情况...');
     }
   } catch (e) {
+    print('request url falied... ${servicePath[url]}error:::${e}');
     return print('error:::${e}');
   }
 }
